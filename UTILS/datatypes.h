eeaf046b53613a4928ca92395bffe92a6b50c826
Mahmoud Moa'wad <41588241+mahmoudmuawad@users.noreply.github.com>
2019-12-18 23:55:52 +0200

    Add files via upload

---
+++ UTILS/datatypes.h
@@ -0,0 +1,20 @@
+#ifndef DATATYPES_H_
+#define DATATYPES_H_
+
+// NOTE :	typedef differs from #define it must be followed by semicolumn ; 
+typedef unsigned char U8;
+typedef signed char S8;
+
+typedef unsigned short U16;
+typedef signed short S16;
+
+typedef unsigned long  U32;
+typedef signed long   S32;
+
+typedef unsigned long long   U64;
+typedef signed long  long    S64;
+
+typedef float F32;
+typedef double F64;
+
+#endif
